52aca72547df6688da5048059c523d56600c5e7c
neymar <neybriramos@gmail.com>
2026-03-16 23:46:19 -0400

    update

--- app/model/verificarModel.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once dirname(__DIR__) . "/config/claseconexion.php";
-
-class Verificar extends Conexion {
-    public function __construct() {
-        parent::__construct();
-        $this->conectar();
-    }
-}
